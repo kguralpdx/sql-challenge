c8fea42b5b9efc05dea6df623a754d563ab738f7
kguralpdx <66285376+kguralpdx@users.noreply.github.com>
2020-08-21 22:38:10 -0700

    added query 6 to queries file

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -70,3 +70,19 @@ WHERE
 ORDER BY
 	last_name;
 
+
+-- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT
+	de.emp_no
+	, e.last_name
+	, e.first_name
+	, d.dept_name
+FROM
+	employees e INNER JOIN dept_emp de
+		ON e.emp_no = de.emp_no
+	INNER JOIN departments d
+		ON de.dept_no = d.dept_no
+WHERE
+	d.dept_name = 'Sales'
+ORDER BY
+	de.emp_no;
