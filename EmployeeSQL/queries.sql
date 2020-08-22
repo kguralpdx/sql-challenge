da8fc799c348c61cbbf4ae99dd28b90c4f38b7ba
kguralpdx <66285376+kguralpdx@users.noreply.github.com>
2020-08-21 22:03:46 -0700

    added the third query expression

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -1,4 +1,4 @@
---List the following details of each employee: employee number, last name, first name, sex, and salary.
+-- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
 SELECT 
 	e.emp_no
 	, e.last_name
@@ -10,8 +10,9 @@ FROM
 		ON e.emp_no = s.emp_no
 ORDER BY
 	e.emp_no
-	
---List first name, last name, and hire date for employees who were hired in 1986.
+
+
+-- 2. List first name, last name, and hire date for employees who were hired in 1986.
 SELECT
 	first_name
 	, last_name
@@ -22,3 +23,23 @@ WHERE
 	extract(year from hire_date) = 1986
 ORDER BY
 	hire_date
+	
+	
+-- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT
+	d.dept_no
+	, d.dept_name
+	, dm.emp_no
+	, e.last_name
+	, e.first_name
+FROM
+	departments d INNER JOIN dept_manager dm
+		ON d.dept_no = dm.dept_no
+	INNER JOIN employees e
+		ON dm.emp_no = e.emp_no
+ORDER BY
+	d.dept_no
+	, dm.emp_no
+
+
+	
