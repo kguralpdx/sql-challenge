e08812c604ce204ac321100bfa93d3a111bf20b6
kguralpdx <66285376+kguralpdx@users.noreply.github.com>
2020-08-21 22:28:46 -0700

    added query 5 to the queries file

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -56,4 +56,17 @@ FROM
 ORDER BY
 	de.emp_no;
 
-	
+
+-- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT
+	first_name
+	, last_name
+	, sex
+FROM
+	employees
+WHERE
+	first_name = 'Hercules'
+	AND last_name LIKE 'B%'
+ORDER BY
+	last_name;
+
